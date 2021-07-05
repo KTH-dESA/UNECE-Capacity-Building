ff6b129936ed03e7f5fd6d44cad78c7be6e596a1
vignesh1987 <vsri@kth.se>
2021-07-05 10:14:02 +0200

    changed graph colours and updated the high demand data file to be the dsame as the reference

--- CB_osemosys/plotting_simple.py
+++ CB_osemosys/plotting_simple.py
@@ -1,7 +1,10 @@
 #Electricity Generation
 production = pd.read_csv('results/ProductionByTechnologyAnnual.csv')
 production =production[production['TECHNOLOGY'].str.startswith('PWR') & (production['TECHNOLOGY'] !='PWRTRN') & (production['FUEL'].str[:3] =='ELC')].groupby(by=['TECHNOLOGY','YEAR']).sum()
-el_generation=ex.bar(production.reset_index(), x='YEAR', y='VALUE', color='TECHNOLOGY',title='Electricity generation',labels={"VALUE": "Peta Joules"},width=500,height=500)
+el_generation=ex.bar(production.reset_index(), x='YEAR', y='VALUE', color='TECHNOLOGY',color_discrete_map={
+                "PWRGAS": "grey",
+                "PWRHYD": "blue",
+                "PWRSOL": "goldenrod"},title='Electricity generation',labels={"VALUE": "Peta Joules"},width=500,height=500)
 el_generation.update_xaxes(dtick=1)
 '''
 #water use graph
@@ -13,7 +16,10 @@ water_use.update_xaxes(dtick=1)
 #Electricity generation per timeslice
 productionts = pd.read_csv('results/ProductionByTechnology.csv')
 productionts =productionts[productionts['TECHNOLOGY'].str.startswith('PWR') & (productionts['TECHNOLOGY'] !='PWRTRN') & (productionts['FUEL'].str[:3] =='ELC') & (productionts['YEAR'] == 2019)].groupby(by=['TECHNOLOGY','TIMESLICE']).sum()
-el_ts=ex.bar(productionts.reset_index(), x='TIMESLICE', y='VALUE', color='TECHNOLOGY',title='Electricity generation by Time Slice (2019)',labels={"VALUE": "Peta Joules"},width=500,height=500)
+el_ts=ex.bar(productionts.reset_index(), x='TIMESLICE', y='VALUE', color='TECHNOLOGY',color_discrete_map={
+                "PWRGAS": "grey",
+                "PWRHYD": "blue",
+                "PWRSOL": "goldenrod"},title='Electricity generation by Time Slice (2019)',labels={"VALUE": "Peta Joules"},width=500,height=500)
 el_ts.update_xaxes(dtick=1)
 el_generation.show()
 #water_use.show()
